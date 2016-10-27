f87f7383f6afbf6b83f6c039cbbd8f5b9604a752
Alexander <biogenez@yandex.ru>
2016-10-27 10:46:02 +0200

    publish: [+] removed empty <p> before figure
    generated from commit 8d38edc1c6f4edd046af488f4146bc2d6d49dd15

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var allParas = document.getElementsByTagName('p');
-//filter by class name if desired...
-for(var i=0;i<allParas.length;i++){
-  if( allParas[i].textContent == "" ||
-   allParas[i].textContent == "\n"){
-    allParas[i].style.display = 'none';
-  }
-}
+// var allParas = document.getElementsByTagName('p');
+// //filter by class name if desired...
+// for(var i=0;i<allParas.length;i++){
+//   if( allParas[i].textContent == "" ||
+//    allParas[i].textContent == "\n"){
+//     allParas[i].style.display = 'none';
+//   }
+// }
